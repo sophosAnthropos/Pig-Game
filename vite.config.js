e1a42ee08ef9f4e8e30d8405de10593fb5110af1
Christopher Krause <krause.m.christopher@gmail.com>
2023-01-02 15:42:44 -0500

    Adjusting Vite config and rebundling

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sophosPigGame/',
   plugins: [react()],
   server: {
     watch: {
